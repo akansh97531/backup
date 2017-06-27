6740b1f7c8f63a97a1279ac067149e57f3da3bce
akansh <akansh97531@gmail.com>
2017-06-27 15:19:53 +0530

    update

--- clix backup/clix.py
+++ clix backup/clix.py
@@ -1,66 +1,81 @@
-import os, sys
+import os
+import sys
 import json
 import xerox
 import pickle
 import argparse
-from .pyxhook import HookManager
-from .gui import clipboard
-import utils
-
 import threading
+try:
+    import utils
+except:
+    import clix.utils as utils
+from pyxhook import HookManager
+from gui import clipboard
 
 # previously logged key
 prev_Key = None
 # path to site package
 curr_dir = os.getcwd()
 
+key_binding = []
 
-# loading key_binding from config file 
-with open(curr_dir + "/clix/config", "rb") as f:
-        key_binding = pickle.load(f)
-
+# loading key_binding from config file
+# try:
+with open(os.path.join(os.path.dirname(__file__), 'config'), "rb") as f:
+    key_binding = pickle.load(f)
 
 # if file does not exist create empty file
-try:    
-    clips_data = open(curr_dir + "/clix/clips_data", "rb")
+try:
+    clips_data = open(os.path.join(os.path.dirname(__file__),
+                      'clips_data'), "rb")
     utils.clips = pickle.load(clips_data)
     clips_data.close()
 except:
-    clips_data = open(curr_dir + "/clix/clips_data", "wb")
     utils.clips = []
-    clips_data.close()
+
 
 class ThreadedKeyBind(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        
+
     def run(self):
         self.new_hook = HookManager()
         self.new_hook.KeyDown = self.OnKeyPress
         self.new_hook.HookKeyboard()
         self.new_hook.start()
+        # self.new_hook.cancel()
 
-    def OnKeyPress(self,event):
+    def OnKeyPress(self, event):
         """
         function called when any key is pressed
         """
         global prev_Key, key_binding
 
-        if event.Key == key_binding[1] and prev_Key == key_binding[0] and utils.active == 0:
+        if event.Key == key_binding[1] and prev_Key == key_binding[0]:
+            if utils.active == 1:
+                utils.active = 0
+            elif utils.active == 0:
+                utils.active = 1
             prev_Key = None
 
         elif event.Key == 'c' and prev_Key == 'Control_L':
             self.text = xerox.paste(xsel=True)
             utils.clips.append(self.text)
-            # pickle clips data 
-            with open(curr_dir + "/clix/clips_data", "wb") as f:
-                pickle.dump(utils.clips, f, protocol = 2)
-            
+            # pickle clips data
+            with open(os.path.join(os.path.dirname(__file__),
+                      'clips_data'), "wb") as f:
+                pickle.dump(utils.clips, f, protocol=2)
+
             print("You just copied: {}".format(self.text))
+
+        elif event.Key == 'z' and prev_Key == 'Control_L':
+            print("can")
+            self.new_hook.cancel()
 
         else:
             prev_Key = event.Key
 
+        return True
 
 
 def _show_available_keybindings():
@@ -80,14 +95,15 @@ def get_current_keybinding():
     temp = {b: a for a, b in utils.available_keys.items()}
     return temp[key_binding[0]] + "+" + temp[key_binding[1]]
 
+
 def create_new_session():
     """
      clear old session
     """
-    with open(curr_dir + "/clix/clips_data", "wb") as f:
-        utils.clips=[]
-        pickle.dump(utils.clips, f, protocol = 2)
-
+    with open(os.path.join(os.path.dirname(__file__),
+              'clips_data'), "wb") as f:
+        utils.clips = []
+        pickle.dump(utils.clips, f, protocol=2)
 
 
 def main():
@@ -97,25 +113,27 @@ def main():
     global key_binding
 
     parser = argparse.ArgumentParser()
-    
+
     help = """Set alternate key binding. Default is LCTRL+SPACE
                 Format :- <KEY1>+<KEY2>. Ex:- RCTRL+RALT .
                 To see availble key bindings use 'clix -a' option"""
-    
-    parser.add_argument("-s", "--set-keybinding", type = str,
-                        default = None, help = help)
+
+    parser.add_argument("-s", "--set-keybinding", type=str,
+                        default=None, help=help)
 
     parser.add_argument("-a", "--show-available-keybindings",
-                        help = "Show available key bindings", action = "store_true")
-    
-    parser.add_argument("-c", "--show-current-keybinding", action = "store_true")
-    
-    parser.add_argument("-n", "--new-session", action = "store_true",
-                         help = "start new session clearing old session")
+                        help="Show available key bindings",
+                        action="store_true")
+
+    parser.add_argument("-c", "--show-current-keybinding",
+                        action="store_true")
+
+    parser.add_argument("-n", "--new-session", action="store_true",
+                        help="start new session clearing old session")
 
     args = parser.parse_args()
     args_dict = vars(args)
-    
+
     if args.show_current_keybinding:
         print("Current key binding is: {}".format(get_current_keybinding()))
         sys.exit()
@@ -123,16 +141,18 @@ def main():
     elif args.show_available_keybindings:
         _show_available_keybindings()
         sys.exit()
-    
+
     elif args.set_keybinding:
         try:
             keys = args_dict['set_keybinding'].split('+')
-            key_binding = [utils.available_keys[keys[0]], utils.available_keys[keys[1]]]
+            key_binding = [utils.available_keys[keys[0]],
+                           utils.available_keys[keys[1]]]
         except KeyError:
             print("Please follow the correct format.")
         else:
-            with open(curr_dir + "/clix/config", "wb") as f:
-                pickle.dump(key_binding, f, protocol = 2)
+            with open(os.path.join(os.path.dirname(__file__),
+                      'config'), "wb") as f:
+                pickle.dump(key_binding, f, protocol=2)
         finally:
             sys.exit()
 
@@ -142,10 +162,10 @@ def main():
 
     # seperate thread because of tkinter mainloop
     # which blocks every other event
-    ThreadedKeyBind().start()
+    t = ThreadedKeyBind().start()
 
     # start gui
-    utils.active=1
+    utils.active = 1
     clipboard(utils.clips)
 
 
--- clix backup/gui.py
+++ clix backup/gui.py
@@ -1,134 +1,184 @@
 from os import path
 from functools import partial
 try:
-	from Tkinter import *
-except ImportError:
-	from tkinter import *
-try:
-	from ScrolledText import ScrolledText
+    import utils
 except:
-	from tkinter.scrolledtext import ScrolledText
-import xerox
-
+    import clix.utils as utils
 import os
+import xerox
+import pickle
+
+try:
+    from Tkinter import *
+except ImportError:
+    from tkinter import *
+try:
+    from ScrolledText import ScrolledText
+except ImportError:
+    from tkinter.scrolledtext import ScrolledText
+
 curr_dir = os.getcwd()
-import utils
 
-
-def clear_session(root):
-	root.destroy()
-	# clear data in file
-	with open(curr_dir + "/clix/clips_data", "wb") as f:
-		print( "session cleared")
-	# clear global clips
-	utils.clips=[]
-	clipboard(utils.clips)
-	
 
 class clipboard():
-	def __init__(self, clips):
-		"""
-		initialization function
-		"""
-		# root (top level element) config
-		H, W = 250, 300
-		self.root = Tk()
-		self.root.title("clix")
-		self.root.minsize(width = W, height = H)
-		self.position_window()
+    def __init__(self, clips):
+        """
+        initialization function
+        """
+        # root (top level element) config
+        H, W = 250, 300
+        self.root = Tk()
+        self.root.title("clix")
+        self.root.minsize(width=W, height=H)
+        self.position_window()
 
-		img = PhotoImage(file = "icon.png")
-		self.root.tk.call('wm', 'iconphoto', self.root._w, img)
+        # when 'X' button is clicked
+        self.root.protocol('WM_DELETE_WINDOW', self.q)
 
-		self.menu_bar=Menu(self.root)
-		self.menu_bar.add_command(label="Clear", command= lambda: clear_session(self.root) )
-		self.root.config(menu=self.menu_bar)
+        img = PhotoImage(file=os.path.join(os.path.dirname(__file__),
+                         "icon.png"))
+        self.root.tk.call('wm', 'iconphoto', self.root._w, img)
 
-		# canvas to hold main scrollbar
-		self.canvas = Canvas(self.root, height = H - 10, width = W - 20)
-		self.canvas.pack(side = RIGHT, expand = True)
+        # add Menubar
+        self.menu_bar = Menu(self.root)
+        self.menu_bar.add_command(label="Clear",
+                                  command=self.clear_session)
+        self.root.config(menu=self.menu_bar)
 
-		# scrollbar config
-		self.scrollbar = Scrollbar(self.root, command = self.canvas.yview)
-		self.scrollbar.pack(side = RIGHT, fill = 'y')
-		self.canvas.configure(yscrollcommand = self.scrollbar.set)
+        # canvas to hold main scrollbar
+        self.canvas = Canvas(self.root, height=H - 10, width=W - 20)
+        self.canvas.pack(side=RIGHT, expand=True)
 
-		# main frame (inside root) config
-		self.mainFrame = Frame(self.root, padx = 5, pady = 5, bg = "white")
-		self.mainFrame.pack(fill = BOTH, expand = True, side = TOP)
+        # scrollbar config
+        scrollbar = Scrollbar(self.root, command=self.canvas.yview)
+        scrollbar.pack(side=RIGHT, fill='y')
+        self.canvas.configure(yscrollcommand=scrollbar.set)
 
-		# canvas window over mainFrame
-		self.canvas.create_window((0,0), window = self.mainFrame, anchor = 'nw')
-		self.mainFrame.bind('<Configure>', self.on_configure)
+        # main frame (inside root) config
+        self.mainFrame = Frame(self.root, padx=5, pady=5)
+        self.mainFrame.pack(fill=BOTH, expand=True, side=TOP)
 
-		# clipboard frames inside main frame
-		self.colors = ['orange', 'tomato', 'gold']
+        # canvas window over mainFramelistbox
+        self.canvas.create_window((0, 0), window=self.mainFrame, anchor='nw')
+        self.mainFrame.bind('<Configure>', self.on_configure)
 
-		self.frames = []
-		self.textBoxes = []
-		self.no_of_clips=len(clips)
+        # clipboard frames inside main frame
+        self.colors = ['orange', 'tomato', 'gold']
+        self.frames = []
+        self.textBoxes = []
+        self.no_of_clips = len(clips)
 
-		for i in range(self.no_of_clips):
-			self.add_new_clip(clips[i],i)
+        # for i in range(self.no_of_clips):
+        self.add_new_clip()
 
-		self.check_new_clip()
+        self.check_new_clip()
 
-		# call mainloop of Tk object
+        # call mainloop of Tk object
 
-		self.root.mainloop()
-		self.root.quit()
-		utils.active=0
+        self.root.mainloop()
+        self.root.quit()
 
+    def check_new_clip(self):
+        '''
+         check if new clip is added
+        '''
+        if utils.active == 1:
+            # make gui visible
+            self.root.deiconify()
+        else:
+            # make gui hide
+            self.root.withdraw()
 
-	def check_new_clip(self):
-		if len(utils.clips)>self.no_of_clips:
-			self.add_new_clip(utils.clips[-1],self.no_of_clips)
-			self.no_of_clips+=1
+        if len(utils.clips) != self.no_of_clips:
+            self.add_new_clip()
+            
 
-		self.mainFrame.after(500,self.check_new_clip)
+        self.mainFrame.after(500, self.check_new_clip)
 
+    def add_new_clip(self):
+        '''
+         destroy frames and add with new clip added
+        '''
+        for frame in self.frames:
+            frame.destroy()
 
-	def add_new_clip(self,clip,i):
-		frame = Frame(self.mainFrame, padx = 5, pady = 5, bg = self.colors[i%3])
-		
-		Button(frame, text = "clip it", font = "Helvetica 12 bold",  
-			    command = partial(self.copy_to_clipboard, i), relief = RAISED, 
-			    padx = 5, pady = 5, bg = 'dark violet', fg = 'white').grid(row = 0, column = 0, ipady = 10)
+        self.frames = []
+        self.textBoxes = []
+        self.no_of_clips = len(utils.clips)
 
-		textBox = ScrolledText(frame, height = 3, width = 20,font = "Helvetica 12 bold")
-		textBox.insert(END, clip)
+        for clip,i in zip(reversed(utils.clips),range(len(utils.clips)) ):            
+            frame = Frame(self.mainFrame, padx=5, pady=5, bg=self.colors[i % 3])
 
-		textBox.grid(row = 0, column = 1, sticky = E, padx = 5)
-		self.textBoxes.append(textBox)
+            Button(frame, text="clip it ", font="Helvetica 12 bold",
+                   command=partial(self.copy_to_clipboard, i), relief=RAISED,
+                   padx=3, pady=3, bg='dark violet', fg='white').grid(
+                       row=0, column=0, ipady=2
+                   )
 
-		frame.pack(fill = 'both', expand = True, pady = 5)
-		self.frames.append(frame)
+            Button(frame, text="delete", font="Helvetica 12 bold",
+                   command=partial(self.delete_frame, len(utils.clips)-i-1), relief=RAISED,
+                   padx=3, pady=3, bg='dark green', fg='white').grid(
+                       row=1, column=0, ipady=2
+                   )
 
+            textBox = ScrolledText(frame, height=4, width=20,
+                                   font="Helvetica 12 bold")
+            textBox.insert(END, clip)
 
-	def copy_to_clipboard(self, idx):
-		"""
-		function to copy text of frame no. idx to clipboard
-		"""
-		text = self.textBoxes[idx].get("1.0", END)
-		xerox.copy(text)
+            textBox.grid(row=0, column=1, rowspan=2, sticky=E, padx=5)
+            self.textBoxes.append(textBox)
 
+            self.no_of_clips = len(utils.clips)
 
-	def on_configure(self, event):
-		"""
-		utility function to enable scrolling over canvas window
-		"""
-		self.canvas.configure(scrollregion = self.canvas.bbox('all'))
+            frame.pack(fill='both', expand=True, pady=5)
+            self.frames.append(frame)
 
+    def clear_session(self):
+        for frame in self.frames:
+            frame.destroy()
 
-	def position_window(self):
-		"""
-		function to position window to current mouse position
-		"""
-		x, y = self.root.winfo_pointerxy()
-		self.root.geometry('+%d+%d' % (x, y))	
+        self.frames = []
+        self.textBoxes = []
+        self.no_of_clips = 0
+        # clear global clips
+        utils.clips = []
+        # clear data in file
+        with open(os.path.join(os.path.dirname(__file__),
+                  'clips_data'), "wb") as f:
+            pickle.dump(utils.clips, f, protocol=2)
+            print("session cleared")
 
+    def copy_to_clipboard(self, idx):
+        """
+        function to copy text of frame no. idx to clipboard
+        """
+        text = self.textBoxes[idx].get("1.0", END)
+        xerox.copy(text)
+
+    def delete_frame(self, idx):
+        """
+        function to remove any frame
+        """
+        del utils.clips[idx]
+
+    def on_configure(self, event):
+        """
+        utility function to enable scrolling over canvas window
+        """
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
+
+    def position_window(self):
+        """
+        function to position window to current mouse position
+        """
+        x, y = self.root.winfo_pointerxy()
+        self.root.geometry('+%d+%d' % (x, y))
+
+    def q(self):
+        print("closed")
+        utils.active -= 1
+        self.root.withdraw()
 
 if __name__ == "__main__":
-	example_clips = ["hello", "copy it"]
-	clipboard(example_clips)
-
+    example_clips = ["hello", "copy it"]
+    clipboard(example_clips)
